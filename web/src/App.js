c1772137ae7f388fb06d759fec665e62908f7195
JP Dillingham <JP.Dillingham@rackspace.com>
2020-03-26 19:54:50 -0500

    add login

--- web/src/App.js
+++ web/src/App.js
@@ -3,25 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 import Greeting from './Greeting';
+import Login from './Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Greeting/>
-      </header>
+      <Login/>
     </div>
   );
 }
