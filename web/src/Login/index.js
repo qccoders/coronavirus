7bcf533942dfa19ae8bf740b2579c6c9a78f442a
JP Dillingham <JP.Dillingham@rackspace.com>
2020-03-26 20:19:06 -0500

    add login form

--- web/src/Login/index.js
+++ web/src/Login/index.js
@@ -1,33 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+import axios from 'axios';
+import { baseUrl } from '../config';
 
-const Login = () => (
-  <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-    <Grid.Column style={{ maxWidth: 450 }}>
-      <Header as='h2' color='teal' textAlign='center'>
-        <Image src='/logo.png' /> Log-in to your account
-      </Header>
-      <Form size='large'>
-        <Segment stacked>
-          <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-          <Form.Input
-            fluid
-            icon='lock'
-            iconPosition='left'
-            placeholder='Password'
-            type='password'
-          />
+const initialState = {
+  email: ''
+};
 
-          <Button color='teal' fluid size='large'>
-            Login
-          </Button>
-        </Segment>
-      </Form>
-      <Message>
-        New to us? <a href='#'>Sign Up</a>
-      </Message>
-    </Grid.Column>
-  </Grid>
-)
+class Login extends Component {
+  state = initialState;
+
+  handleChange = (e, { value }) => {
+    this.setState({ email: value }, () => console.log(this.state));
+  }
+
+  handleSubmit = () => {
+    axios.post(`${baseUrl}/login`, { id: this.state.email })
+      .then(response => response.data)
+      .then(token => {
+        localStorage.setItem('coronavirus-token', token)
+        console.log('logged in', token);
+      });
+  }
+
+  render() {
+    return (
+      <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as='h2' color='teal' textAlign='center'>
+            Log-in
+          </Header>
+          <Form onSubmit={this.handleSubmit} size='large'>
+            <Segment stacked>
+              <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' onChange={this.handleChange} />
+              <Button color='teal' fluid size='large'>
+                Login
+              </Button>
+            </Segment>
+          </Form>
+        </Grid.Column>
+      </Grid>
+    )
+  }
+}
 
 export default Login
