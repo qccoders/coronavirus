86f4cc01374e6379a81cf162d9c6bcf9c0476fa6
JP Dillingham <JP.Dillingham@rackspace.com>
2020-04-02 19:02:56 -0500

    add questionnaire

---
+++ web/src/Approval/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Approval = ({ code }) => (
+  <>
+    <h1>Your Test Code</h1>
+    <h2>{code}</h2>
+  </>
+);
+
+export default Approval
--- web/src/Dashboard/index.js
+++ web/src/Dashboard/index.js
@@ -1,10 +1,67 @@
 import React, { Component } from 'react'
+import {
+  Button
+} from 'semantic-ui-react';
+
+import Approval from '../Approval';
+
+const initialState = {
+  step: 0,
+  rejected: false,
+  approved: false,
+  code: undefined
+};
+
+const steps = [
+  'Do you have a fever?',
+  'Do you have a cough?',
+  'Are you experiencing shortness of breath?'
+];
 
 class Dashboard extends Component {
-  render() {
+  state = initialState;
+
+  getCode = () => {
+    return 1
+  }
+
+  handleYes = () => {
+    const { step } = this.state;
+
+    if (step === steps.length - 1) {
+      this.setState({ approved: true, code: this.getCode() }, () => {
+
+      });
+    } else {
+      this.setState({ step: step + 1 });
+    }
+  }
+
+  handleNo = () => {
+    this.setState({ rejected: true });
+  }
+
+  renderRejected = () => {
     return (
-      <h1>dashboard</h1>
-    )
+      <div>You don't qualify for a test at this time.</div>
+    );
+  };
+
+  render() {
+    const { rejected, approved, code } = this.state;
+
+    return (
+      <div>
+        <h1>Coronavirus Screening</h1>
+        {rejected ? this.renderRejected() :
+          approved ? <Approval code={code}/> :
+            <div>
+              <h2>{steps[this.state.step]}</h2>
+              <Button onClick={this.handleYes}>Yes</Button><Button onClick={this.handleNo}>No</Button>
+            </div>
+        }
+      </div>
+    );
   }
 }
 
